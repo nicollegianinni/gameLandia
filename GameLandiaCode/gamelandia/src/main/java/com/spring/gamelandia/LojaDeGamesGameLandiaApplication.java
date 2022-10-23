ab1c9cde407e004e5e2cca6994af068f31d4fd3f
Nicolle Gianinni <nicolle.gianinni@hotmail.com>
2022-10-22 22:47:03 -0300

    criacaodoprojeto (#1)

---
+++ GameLandiaCode/gamelandia/src/main/java/com/spring/gamelandia/LojaDeGamesGameLandiaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.gamelandia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeGamesGameLandiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeGamesGameLandiaApplication.class, args);
+	}
+
+}
